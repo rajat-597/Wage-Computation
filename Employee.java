93e8454cc0fec712aabf8bf99a179cefc918bc99
Rajat <rksahoome140063@gmail.com>
2020-10-30 13:42:41 +0530

    Added new Features

--- Employee.java
+++ Employee.java
@@ -5,7 +5,20 @@ public class Employee {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Employee Wage Computatation program");
-
+		Employee e = new Employee();
+		e.check();
 	}
+public void check()
+{
+	double d = Math.random();
+	if(d==0)
+	{
+		System.out.println("Employee is not present");
+	}
+	else
+	{
+		System.out.println("Employee is present");
+	}
+}
 
 }
